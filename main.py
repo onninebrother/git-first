5232c0198844ffa130637353ce9c2084fc55d008
gin <123@123.com>
2023-03-25 13:34:06 +0800

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_hi(name):
+    print("hello,world")
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
