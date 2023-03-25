e23d2e4b09c3ceede98bf3950d8cbb1b957c9a29
gin <123@123.com>
2023-03-25 13:49:38 +0800

    添加 加法

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def print_hi(name):
-    print("hello,world")
+    print(add(7))
 
-
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
+def printttt(a, b):
+    return a+b
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+def add(a):
+    a += a
+    return a
